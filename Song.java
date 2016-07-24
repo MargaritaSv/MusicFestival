04a8df6e79ecd0e0ce0f4bf9a4a716cf9c7cfbba
MargaritaSv <margarita.esti@gmail.com>
2016-07-24 08:26:49 +0300

    Condition on Music Festival.pdf. Create class Song.java.

---
+++ Song.java
@@ -0,0 +1,42 @@
+package MusicFestival;
+
+/**
+ * Created by Magi on 24.7.2016 г..
+ */
+public class Song {
+    private String title;
+    private StringBuilder text;
+
+    public Song(String title, StringBuilder text) {
+        this.setTitle(title);
+        this.setText(text);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.trim().length() == 0) {
+            throw new IllegalArgumentException("The name of the song is missing.");
+        }
+        this.title = title;
+    }
+
+    public StringBuilder getText() {
+        return text;
+    }
+
+    public void setText(StringBuilder text) {
+        if (text.equals(null) || text.length() == 0) {
+            throw new IllegalArgumentException("Song need a text even chalga style :D.");
+
+        }
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "The song: " + this.getTitle() + "\n" + this.getText();
+    }
+}
